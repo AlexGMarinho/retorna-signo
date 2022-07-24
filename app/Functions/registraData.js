bbac9b760d51c325d05e13ebfbd8b12630ec9778
AlexGMarinho <alexg.marinhoooo>
2022-07-24 18:29:42 -0300

    registraData.js

---
+++ app/Functions/registraData.js
@@ -0,0 +1,19 @@
+import readlineSync from 'readline-sync';
+
+function diaRegistrado() 
+{
+    console.log(`=== Digite o Dia e Mês que você nasceu ===`);
+
+    let dia = readlineSync.questionFloat('Dia: ');
+
+    return dia;
+}
+
+function mesRegistrado() 
+{
+    let mes = readlineSync.questionFloat('Mes: ');
+
+    return mes;
+}
+
+export { diaRegistrado, mesRegistrado };
