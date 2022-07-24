a2c9a2579bffbfd1d4c31d5ededf627bad276b86
AlexGMarinho <alexg.marinhoooo>
2022-07-24 18:30:06 -0300

    retornaDataNascimento.js

---
+++ app/Functions/retornaDataNascimento.js
@@ -0,0 +1,13 @@
+import {diaRegistrado, mesRegistrado} from './registraData.js'
+
+function dataNascimento() 
+{
+    let dataNascimento = new Date();
+
+    dataNascimento.setDate(diaRegistrado());
+    dataNascimento.setMonth(mesRegistrado() - 1);
+
+    return dataNascimento;
+}
+
+export default dataNascimento;
